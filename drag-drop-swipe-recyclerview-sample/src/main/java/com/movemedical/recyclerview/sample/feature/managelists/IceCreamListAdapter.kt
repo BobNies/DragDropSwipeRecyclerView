466e2bc28dfe61a7c54065763c368e8f2f077a18
Bob Nies <bob.nies@movemedical.com>
2019-10-23 13:16:06 -0700

    package renaming

---
+++ drag-drop-swipe-recyclerview-sample/src/main/java/com/movemedical/recyclerview/sample/feature/managelists/IceCreamListAdapter.kt
@@ -0,0 +1,101 @@
+package com.movemedical.recyclerview.sample.feature.managelists
+
+import android.content.res.ColorStateList
+import android.graphics.Canvas
+import android.graphics.Color
+import android.view.LayoutInflater
+import androidx.core.widget.ImageViewCompat
+import androidx.appcompat.widget.AppCompatImageView
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import com.movemedical.recyclerview.DragDropSwipeAdapter
+import com.movemedical.recyclerview.sample.R
+import com.movemedical.recyclerview.sample.data.model.IceCream
+import com.movemedical.recyclerview.sample.util.Logger
+
+/**
+ * Adapter for a list of ice creams.
+ */
+class IceCreamListAdapter(dataSet: List<IceCream> = emptyList())
+    : DragDropSwipeAdapter<IceCream, IceCreamListAdapter.ViewHolder>(dataSet) {
+
+    class ViewHolder(iceCreamLayout: View) : DragDropSwipeAdapter.ViewHolder(iceCreamLayout) {
+        val iceCreamNameView: TextView = itemView.findViewById(R.id.ice_cream_name)
+        val iceCreamPriceView: TextView = itemView.findViewById(R.id.ice_cream_price)
+        val dragIcon: AppCompatImageView = itemView.findViewById(R.id.drag_icon)
+        val iceCreamIcon: AppCompatImageView? = itemView.findViewById(R.id.ice_cream_icon)
+        val iceCreamPhotoFilter: View? = itemView.findViewById(R.id.ice_cream_photo_filter)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+                    val itemLayout = LayoutInflater.from(parent.context)
+                    .inflate(R.layout.list_item_vertical_list, parent, false) as View
+
+            return ViewHolder(itemLayout)
+    }
+
+    override fun onBindViewHolder(item: IceCream, viewHolder: ViewHolder, position: Int) {
+        val context = viewHolder.itemView.context
+
+        // Set ice cream name and price
+        viewHolder.iceCreamNameView.text = item.name
+        viewHolder.iceCreamPriceView.text = context.getString(R.string.priceFormat, item.price)
+
+        // Set ice cream icon color
+        val red = (item.colorRed * 255).toInt()
+        val green = (item.colorGreen * 255).toInt()
+        val blue = (item.colorBlue * 255).toInt()
+
+        // Set the icon/image color
+        if (viewHolder.iceCreamIcon != null) {
+            val iceCreamIconColor = Color.rgb(red, green, blue)
+            ImageViewCompat.setImageTintList(viewHolder.iceCreamIcon, ColorStateList.valueOf(iceCreamIconColor))
+        } else if (viewHolder.iceCreamPhotoFilter != null) {
+            val iceCreamPhotoFilter = Color.argb(128, red, green, blue)
+            viewHolder.iceCreamPhotoFilter.setBackgroundColor(iceCreamPhotoFilter)
+        }
+    }
+
+    override fun getViewToTouchToStartDraggingItem(item: IceCream, viewHolder: ViewHolder, position: Int) = viewHolder.dragIcon
+
+    override fun onDragStarted(item: IceCream, viewHolder: ViewHolder) {
+        Logger.log("Dragging started on ${item.name}")
+    }
+
+    override fun onSwipeStarted(item: IceCream, viewHolder: ViewHolder) {
+        Logger.log("Swiping started on ${item.name}")
+    }
+
+    override fun onIsDragging(
+            item: IceCream?,
+            viewHolder: ViewHolder,
+            offsetX: Int,
+            offsetY: Int,
+            canvasUnder: Canvas?,
+            canvasOver: Canvas?,
+            isUserControlled: Boolean) {
+        // Call commented out to avoid saturating the log
+        //Logger.log("The ${if (isUserControlled) "User" else "System"} is dragging ${item.name} (offset X: $offsetX, offset Y: $offsetY)")
+    }
+
+    override fun onIsSwiping(
+            item: IceCream?,
+            viewHolder: ViewHolder,
+            offsetX: Int,
+            offsetY: Int,
+            canvasUnder: Canvas?,
+            canvasOver: Canvas?,
+            isUserControlled: Boolean) {
+        // Call commented out to avoid saturating the log
+        //Logger.log("The ${if (isUserControlled) "User" else "System"} is swiping ${item?.name} (offset X: $offsetX, offset Y: $offsetY)")
+    }
+
+    override fun onDragFinished(item: IceCream, viewHolder: ViewHolder) {
+        Logger.log("Dragging finished on ${item.name} (the item was dropped)")
+    }
+
+    override fun onSwipeAnimationFinished(viewHolder: ViewHolder) {
+        Logger.log("Swiping animation finished")
+    }
+}
---
+++ drag-drop-swipe-recyclerview-sample/src/main/java/com/movemedical/recyclerview/sample/feature/managelists/view/HorizontalListFragment.kt
@@ -0,0 +1,106 @@
+package com.movemedical.recyclerview.sample.feature.managelists.view
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.movemedical.recyclerview.DragDropSwipeRecyclerView
+import com.movemedical.recyclerview.sample.R
+import com.movemedical.recyclerview.sample.config.local.currentListFragmentConfig
+import com.movemedical.recyclerview.sample.databinding.FragmentHorizontalListBinding
+import com.movemedical.recyclerview.sample.feature.managelists.view.base.BaseListFragment
+
+
+class HorizontalListFragment : BaseListFragment() {
+
+    private lateinit var binding: FragmentHorizontalListBinding
+    override val optionsMenuId = R.menu.fragment_horizontal_list_options
+
+    override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?): View? {
+
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_horizontal_list, container, false)
+        binding.lifecycleOwner = viewLifecycleOwner
+
+        loadingIndicator = binding.loadingIndicator
+
+        list = binding.list
+        list.adapter = adapter
+        list.swipeListener = onItemSwipeListener
+        list.dragListener = onItemDragListener
+        list.scrollListener = onListScrollListener
+
+        list.layoutManager = LinearLayoutManager(activity, RecyclerView.HORIZONTAL, false)
+        list.reduceItemAlphaOnSwiping = currentListFragmentConfig.isUsingFadeOnSwipedItems
+
+        list.orientation = if (currentListFragmentConfig.isRestrictingDraggingDirections) {
+
+            DragDropSwipeRecyclerView.ListOrientation.HORIZONTAL_LIST_WITH_HORIZONTAL_DRAGGING
+        } else {
+            DragDropSwipeRecyclerView.ListOrientation.HORIZONTAL_LIST_WITH_UNCONSTRAINED_DRAGGING
+        }
+
+        setupLayoutBehindItemLayoutOnSwiping()
+
+        return binding.root
+    }
+
+    private fun setupLayoutBehindItemLayoutOnSwiping() {
+        // We set to null all the properties that can be used to display something behind swiped items
+        // In XML: app:behind_swiped_item_bg_color="@null"
+        list.behindSwipedItemBackgroundColor = null
+
+        // In XML: app:behind_swiped_item_bg_color_secondary="@null"
+        list.behindSwipedItemBackgroundSecondaryColor = null
+
+        // In XML: app:behind_swiped_item_icon="@null"
+        list.behindSwipedItemIconDrawableId = null
+
+        // In XML: app:behind_swiped_item_icon_secondary="@null"
+        list.behindSwipedItemIconSecondaryDrawableId = null
+
+        // In XML: app:behind_swiped_item_custom_layout="@null"
+        list.behindSwipedItemLayoutId = null
+
+        // In XML: app:behind_swiped_item_custom_layout_secondary="@null"
+        list.behindSwipedItemSecondaryLayoutId = null
+
+        val currentContext = context
+        if (currentListFragmentConfig.isDrawingBehindSwipedItems && currentContext != null)
+            if (currentListFragmentConfig.isUsingStandardItemLayout) {
+                // We set certain properties to show an icon and a background colour behind swiped items
+                // In XML: app:behind_swiped_item_icon="@drawable/ic_remove_item"
+                list.behindSwipedItemIconDrawableId = R.drawable.ic_remove_item
+
+                // In XML: app:behind_swiped_item_icon_secondary="@drawable/ic_archive_item"
+                list.behindSwipedItemIconSecondaryDrawableId = R.drawable.ic_archive_item
+
+                // In XML: app:behind_swiped_item_bg_color="@color/swipeBehindBackground"
+                list.behindSwipedItemBackgroundColor = ContextCompat.getColor(currentContext, R.color.swipeBehindBackground)
+
+                // In XML: app:behind_swiped_item_bg_color_secondary="@color/swipeBehindBackgroundSecondary"
+                list.behindSwipedItemBackgroundSecondaryColor = ContextCompat.getColor(currentContext, R.color.swipeBehindBackgroundSecondary)
+
+                // In XML: app:behind_swiped_item_icon_centered="true"
+                list.behindSwipedItemCenterIcon = true
+            } else {
+                // We set our custom layouts to be displayed behind swiped items
+                // In XML: app:behind_swiped_item_custom_layout="@layout/behind_swiped_horizontal_list"
+                list.behindSwipedItemLayoutId = R.layout.behind_swiped_horizontal_list
+
+                // In XML: app:behind_swiped_item_custom_layout_secondary="@layout/behind_swiped_horizontal_list_secondary"
+                list.behindSwipedItemSecondaryLayoutId = R.layout.behind_swiped_horizontal_list_secondary
+            }
+    }
+
+
+    companion object {
+        fun newInstance() = HorizontalListFragment()
+    }
+}
