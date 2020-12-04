2610cef494db29d079a5e5bdf9ce75c6b62f6c90
bobNies <bob.nies@movemedical.com>
2020-12-04 10:26:24 -0800

    fix sample app

--- drag-drop-swipe-recyclerview-sample/src/main/java/com/movemedical/recyclerview/sample/view/ui/HorizontalListFragment.kt
+++ drag-drop-swipe-recyclerview-sample/src/main/java/com/movemedical/recyclerview/sample/view/ui/HorizontalListFragment.kt
@@ -52,48 +52,48 @@ class HorizontalListFragment : BaseListFragment() {
 
     private fun setupLayoutBehindItemLayoutOnSwiping() {
         // We set to null all the properties that can be used to display something behind swiped items
-        // In XML: app:behind_swiped_item_bg_color="@null"
+        // In XML: app:swiped_item_bg_color="@null"
         list.behindSwipedItemBackgroundColor = null
 
-        // In XML: app:behind_swiped_item_bg_color_secondary="@null"
+        // In XML: app:swiped_item_bg_color_secondary="@null"
         list.behindSwipedItemBackgroundSecondaryColor = null
 
-        // In XML: app:behind_swiped_item_icon="@null"
+        // In XML: app:swiped_item_icon="@null"
         list.behindSwipedItemIconDrawableId = null
 
-        // In XML: app:behind_swiped_item_icon_secondary="@null"
+        // In XML: app:swiped_item_icon_secondary="@null"
         list.behindSwipedItemIconSecondaryDrawableId = null
 
-        // In XML: app:behind_swiped_item_custom_layout="@null"
+        // In XML: app:swiped_item_custom_layout="@null"
         list.behindSwipedItemLayoutId = null
 
-        // In XML: app:behind_swiped_item_custom_layout_secondary="@null"
+        // In XML: app:swiped_item_custom_layout_secondary="@null"
         list.behindSwipedItemSecondaryLayoutId = null
 
         val currentContext = context
         if (currentListFragmentConfig.isDrawingBehindSwipedItems && currentContext != null)
             if (currentListFragmentConfig.isUsingStandardItemLayout) {
                 // We set certain properties to show an icon and a background colour behind swiped items
-                // In XML: app:behind_swiped_item_icon="@drawable/ic_remove_item"
+                // In XML: app:swiped_item_icon="@drawable/ic_remove_item"
                 list.behindSwipedItemIconDrawableId = R.drawable.ic_remove_item
 
-                // In XML: app:behind_swiped_item_icon_secondary="@drawable/ic_archive_item"
+                // In XML: app:swiped_item_icon_secondary="@drawable/ic_archive_item"
                 list.behindSwipedItemIconSecondaryDrawableId = R.drawable.ic_archive_item
 
-                // In XML: app:behind_swiped_item_bg_color="@color/swipeBehindBackground"
+                // In XML: app:swiped_item_bg_color="@color/swipeBehindBackground"
                 list.behindSwipedItemBackgroundColor = ContextCompat.getColor(currentContext, R.color.swipeBehindBackground)
 
-                // In XML: app:behind_swiped_item_bg_color_secondary="@color/swipeBehindBackgroundSecondary"
+                // In XML: app:swiped_item_bg_color_secondary="@color/swipeBehindBackgroundSecondary"
                 list.behindSwipedItemBackgroundSecondaryColor = ContextCompat.getColor(currentContext, R.color.swipeBehindBackgroundSecondary)
 
-                // In XML: app:behind_swiped_item_icon_centered="true"
+                // In XML: app:swiped_item_icon_centered="true"
                 list.behindSwipedItemCenterIcon = true
             } else {
                 // We set our custom layouts to be displayed behind swiped items
-                // In XML: app:behind_swiped_item_custom_layout="@layout/behind_swiped_horizontal_list"
+                // In XML: app:swiped_item_custom_layout="@layout/swiped_horizontal_list"
                 list.behindSwipedItemLayoutId = R.layout.behind_swiped_horizontal_list
 
-                // In XML: app:behind_swiped_item_custom_layout_secondary="@layout/behind_swiped_horizontal_list_secondary"
+                // In XML: app:swiped_item_custom_layout_secondary="@layout/swiped_horizontal_list_secondary"
                 list.behindSwipedItemSecondaryLayoutId = R.layout.behind_swiped_horizontal_list_secondary
             }
     }
